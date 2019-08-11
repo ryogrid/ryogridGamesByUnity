1408061b15604c45455954c3e50467db5bb592a7
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-08-11 14:14:13 +0900

    3rd chapter finished

---
+++ Assets/BulletController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+	void Update () {
+		transform.Translate (0, 0.2f, 0);
+
+		if (transform.position.y > 5) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/RockGenerator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockGenerator : MonoBehaviour {
+
+	public GameObject rockPrefab;
+
+	void Start () {
+		InvokeRepeating ("GenRock", 1, 1);
+	}
+	
+	void GenRock () {
+		Instantiate (rockPrefab, new Vector3 (-2.5f + 5 * Random.value, 6, 0), Quaternion.identity);
+	}
+}
