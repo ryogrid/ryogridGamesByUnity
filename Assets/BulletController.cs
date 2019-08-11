b3b1a6e6f8545926f310977654d06f93f76fb2e1
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-08-11 15:24:56 +0900

    chapter 4 finished (explosion effect is not shown)

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -2,11 +2,21 @@
 using System.Collections;
 
 public class BulletController : MonoBehaviour {
+
+	public GameObject explosionPrefab;   //爆発エフェクトのPrefab
+
 	void Update () {
 		transform.Translate (0, 0.2f, 0);
 
 		if (transform.position.y > 5) {
 			Destroy (gameObject);
 		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+	    // 爆発エフェクトを生成する	
+		Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+		Destroy (coll.gameObject);
+		Destroy (gameObject);
 	}
 }
