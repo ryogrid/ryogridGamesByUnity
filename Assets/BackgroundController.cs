fc1a45c46bb2b15ab948b63249fd6057e7dc3793
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-08-11 18:57:45 +0900

    chapter 6 finished (trail effect is not worked)

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+	void Update () {
+		transform.Translate (0, -0.03f, 0);
+		if (transform.position.y < -4.9f) {
+			transform.position = new Vector3 (0, 4.9f, 0);
+		}
+	}
+}
