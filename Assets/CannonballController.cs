d5ea162ba29d503856b920b23c5e8e6331053802
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-08-12 13:27:08 +0900

    Toride0

---
+++ Assets/CannonballController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonballController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.GetComponent<Rigidbody>().AddForce(bulletInstance.transform.forward * bulletPower);
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-500, 700));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -8) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/KeyProssChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyProssChecker : MonoBehaviour
+{
+
+    public GameObject CannonballPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("checkKeyPress", 0.01f, 0.01f);
+    }
+
+    
+    void checkKeyPress()
+    {
+ 		if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate (CannonballPrefab, new Vector3 (8.5f, -4.5f, 0), Quaternion.identity);
+		}       
+    }
+}
