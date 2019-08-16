133e02c6ed03ac85e2f1362ceb5f1d3e85127a57
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-08-16 19:58:30 +0900

    implemented: bounce of canonbool.

--- Assets/MatoController.cs
+++ Assets/MatoController.cs
@@ -20,8 +20,21 @@ public class MatoController : MonoBehaviour
         
     }
 
+/*
 	void OnTriggerEnter2D(Collider2D coll) {
-		Destroy (coll.gameObject);
+		//Destroy (coll.gameObject);
+		Destroy (gameObject);
+        //Instantiate(MatoPrefab, new Vector3(Random.Range(-7f, 7f), Random.Range(-4f, 4f), 0), Quaternion.identity);
+        //Instantiate(gameObject, new Vector3(Random.Range(-7f, 7f), Random.Range(-4f, 4f), 0), Quaternion.identity);
+
+        GameObject MessageText = GameObject.Find("MessageText");
+        MessageText.GetComponent<Text>().text = "君、意外とやるじゃん";
+    }
+*/
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+		//Destroy (coll.gameObject);
 		Destroy (gameObject);
         //Instantiate(MatoPrefab, new Vector3(Random.Range(-7f, 7f), Random.Range(-4f, 4f), 0), Quaternion.identity);
         //Instantiate(gameObject, new Vector3(Random.Range(-7f, 7f), Random.Range(-4f, 4f), 0), Quaternion.identity);
