f0a749c1189fa4fd36bed4f3a703566c9f27e2fa
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-08-25 09:40:17 +0900

    implemnted self bound logc and bound works expectedlly.

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     private Rigidbody rb;
+    private Vector3 lastVelocity;
     private Vector3 lastPosition;
     private int counter = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-         this.rb = this.GetComponent<Rigidbody>();       
+         rb = this.GetComponent<Rigidbody>();       
+    }
+
+    //一定秒数ごとに呼び出される. デフォルトは20ms間隔らしい
+    void FixedUpdate()
+    {
+        lastVelocity = rb.velocity;
     }
 
     // Update is called once per frame
@@ -30,8 +37,16 @@ public class BallController : MonoBehaviour
         }
 
         if (counter % 17 == 0 && lastPosition.x == gameObject.transform.position.x &&  lastPosition.y == gameObject.transform.position.y && lastPosition.z == gameObject.transform.position.z
-            && this.rb.velocity.x == 0 && this.rb.velocity.x == 0 && this.rb.velocity.z == 0){
+            && rb.velocity.x == 0 && rb.velocity.x == 0 && rb.velocity.z == 0){
             Destroy(gameObject);
         }
     }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        Vector3 refrectVec = Vector3.Reflect(this.lastVelocity, coll.contacts[0].normal);
+        Debug.Log(refrectVec);
+        rb.velocity = refrectVec;
+    }
+
 }
