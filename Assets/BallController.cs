9d3d96e81aa616c6647539af17ea2be2c2e95939
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-08-25 17:15:44 +0900

    implemented ball x-y position indecate line and worked it.

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     private Rigidbody rb;
+    private GameObject BallZLine;
     private Vector3 lastVelocity;
     private Vector3 lastPosition;
     private int counter = 0;
@@ -12,13 +13,22 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         rb = this.GetComponent<Rigidbody>();    
+        rb = this.GetComponent<Rigidbody>();
+        BallZLine = GameObject.Find("BallZLine");
+
     }
 
     //一定秒数ごとに呼び出される. デフォルトは20ms間隔らしい
     void FixedUpdate()
     {
         lastVelocity = rb.velocity;
+        //iTween.MoveTo(BallZLine, iTween.Hash("x", this.transform.position.x, "y", this.transform.position.y));
+        BallZLine.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+    }
+
+    private void resetBallZLine()
+    {
+        BallZLine.transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
@@ -26,6 +36,7 @@ public class BallController : MonoBehaviour
     {
         if (transform.position.z < -3f)
         {
+            resetBallZLine();
             Destroy(gameObject);
         }
 
@@ -38,6 +49,7 @@ public class BallController : MonoBehaviour
 
         if (counter % 17 == 0 && lastPosition.x == gameObject.transform.position.x &&  lastPosition.y == gameObject.transform.position.y && lastPosition.z == gameObject.transform.position.z
             && rb.velocity.x == 0 && rb.velocity.x == 0 && rb.velocity.z == 0){
+            resetBallZLine();
             Destroy(gameObject);
         }
     }
